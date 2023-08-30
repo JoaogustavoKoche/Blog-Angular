2c6c02433bcbe1d982476ed7840b25f70561e7c4
JoaogustavoKoche <joaogustavokoche@gmail.com>
2023-08-30 12:09:42 -0300

    dinamycs data

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "title":"Remake de RDR 2",
+        "description":"Rockstar anuncia Remake de RDR 2",
+        "photoCover":"https://compass-ssl.xbox.com/assets/fd/a0/fda0b029-a9d3-4780-a842-9c885b19198b.jpg?n=Red-Dead-Redemption-II_Gallery-0_1350x759_02.jpg"
+    },
+    {
+        "id":"2",
+        "title":"Novo RDR anunciado",
+        "description":"Remake de rdr",
+        "photoCover":"https://cdn.tecmasters.com.br/wp-content/uploads/2022/07/Red-Dead-Revolver-Unreal-Engine-5.jpg"
+    },
+    {
+        "id":"3",
+        "title":"Novo Remake de RDR 1",
+        "description":"Novo Remake de RDR 1",
+        "photoCover":"https://eu-images.contentstack.com/v3/assets/bltbc1876152fcd9f07/bltecd5191f466b82bb/6437db8d72929ed567fa2489/red_dead_redemption_2_remake.png"
+    },
+    {
+        "id":"4",
+        "title":"O fim do RDR Online",
+        "description":"O fim do RDR Online",
+        "photoCover":"https://www.gameshub.com/wp-content/uploads/sites/5/2022/07/red-dead-online-game.jpeg"
+    }
+]
